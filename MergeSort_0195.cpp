d0b3872af52e60a0f90da4b6f215654ce0bb6eb5
Aseel <asilalhashmi@email.com>
2026-04-07 14:15:07 +0700

    add input function

--- MergeSort_0195.cpp
+++ MergeSort_0195.cpp
@@ -4,6 +4,33 @@ using namespace std;
 
 int arr[20], B[8];
 int n;
+void input()
+{
+    while (true)
+    {
+        cout << "Enter the number of elements in the array: ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaximum array length is 20" << endl;
+        }
+    }
+
+    cout << "\n-----------------------------" << endl;
+    cout << "Enter array elements" << endl;
+    cout << "-----------------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array index " << i << ": ";
+        cin >> arr[i];
+    }
+}
 int main()
 {
  
